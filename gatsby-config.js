301797fdf0a42da6d781e5416a2596894dd39bde
Bhavi Dhingra <bhavi.dhingra1@swiggy.in>
2021-03-10 21:35:23 +0530

    correcting GA_TRACKING_ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'none',
+        trackingId: process.env.GA_TRACKING_ID || 'none',
         head: true,
         anonymize: true,
       },
